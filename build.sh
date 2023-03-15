100d9a1bea9a1fd5fe1a8febbc8526e4198e9228
Yoiner Duran <duranyoiner@gmail.com>
2023-03-15 16:29:01 -0500

    requi

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ set -o errexit
 
 # poetry install
 # para que en la nube, se instale esos requerimientos de mi proyecto, se saca con "pip freeze > requeriments.txt"
-# pip install -r requierements.txt
+pip install -r requierements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
