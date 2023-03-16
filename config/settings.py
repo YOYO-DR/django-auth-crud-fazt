a2b0e10011c2f5a616b4c9a66906bd869332db6b
Yoiner Duran <duranyoiner@gmail.com>
2023-03-15 19:47:28 -0500

    config static

--- config/settings.py
+++ config/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'staticfiles/'
+STATIC_URL = '/static/'
 
 if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
@@ -142,10 +142,7 @@ if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-else:
-    STATICFILES_DIRS = [
-  os.path.join(BASE_DIR,"staticfiles")
-  ]
+
 LOGIN_URL = 'signin'
 
 # Default primary key field type
