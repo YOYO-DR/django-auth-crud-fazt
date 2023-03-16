12576d67fe9af848a5d1d3fd568deb42f06c280e
Yoiner Duran <duranyoiner@gmail.com>
2023-03-15 19:17:59 -0500

    config stattic files en nube y local con condicional

--- config/settings.py
+++ config/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'staticfiles/'
 
 if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
