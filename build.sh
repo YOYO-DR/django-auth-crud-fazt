668198762a7cb0f6bac07d02a8160c986b39bd6a
Yoiner Duran <duranyoiner@gmail.com>
2023-03-17 02:51:03 -0500

    prueba

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 # exit on error
 set -o errexit
 
-# poetry install 
+# poetry install
 # para que en la nube, se instale esos requerimientos de mi proyecto, se saca con "pip freeze > requeriments.txt"
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
